b853efa24454b2aef585b1e2236834472b6f4a28
Gabe Nielsen <gabenielsen7@gmail.com>
2022-03-23 11:45:59 -0700

    added calculator function
    Co-authored-by: Cameron Ray <crayiphone@gmail.com
    >

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,41 @@ function subtract(number1, number2) {
   return number1 - number2;
 }
 
-function devide(number1, number2) {
-  return number1 / number2;
-}
-
 function multiply(number1, number2) {
   return number1 * number2;
 }
 
+function divide(number1, number2) {
+  return number1 / number2;
+}
 
-
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2));
+$(document).ready(function () {
+  $("form#add").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#sum").text("The sum is: " + result);
+  });
+  $("form#subtract").submit(function (event) {
+    event.preventDefault();
+    const num1 = parseInt($("#subtract1").val());
+    const num2 = parseInt($("#subtract2").val());
+    const result = subtract(num1, num2);
+    $("#difference").text("The difference is: " + result);
+  });
+  $("form#multiply").submit(function (event) {
+    event.preventDefault();
+    const num1 = parseInt($("#multiply1").val());
+    const num2 = parseInt($("#multiply2").val());
+    const result = multiply(num1, num2);
+    $("#product").text("The product is: " + result);
+  });
+  $("form#divide").submit(function (event) {
+    event.preventDefault();
+    const num1 = parseInt($("#divide1").val());
+    const num2 = parseInt($("#divide2").val());
+    const result = divide(num1, num2);
+    $("#quotient").text("The quotient is: " + result);
+  });
+});
