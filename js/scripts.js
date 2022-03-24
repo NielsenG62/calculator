491f99ffa8a0c9d50d7cfaa890785aad94f67eed
Gabe Nielsen <gabenielsen7@gmail.com>
2022-03-24 14:43:54 -0700

    condense calculator to one form

--- js/scripts.js
+++ js/scripts.js
@@ -15,32 +15,23 @@ function divide(number1, number2) {
 }
 
 $(document).ready(function () {
-  $("form#add").submit(function (event) {
+  $("form#calculator").submit(function (event) {
     event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
-    $("#sum").text("The sum is: " + result);
-  });
-  $("form#subtract").submit(function (event) {
-    event.preventDefault();
-    const num1 = parseInt($("#subtract1").val());
-    const num2 = parseInt($("#subtract2").val());
-    const result = subtract(num1, num2);
-    $("#difference").text("The difference is: " + result);
-  });
-  $("form#multiply").submit(function (event) {
-    event.preventDefault();
-    const num1 = parseInt($("#multiply1").val());
-    const num2 = parseInt($("#multiply2").val());
-    const result = multiply(num1, num2);
-    $("#product").text("The product is: " + result);
-  });
-  $("form#divide").submit(function (event) {
-    event.preventDefault();
-    const num1 = parseInt($("#divide1").val());
-    const num2 = parseInt($("#divide2").val());
-    const result = divide(num1, num2);
-    $("#quotient").text("The quotient is: " + result);
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result = "";
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    } else {
+      return "Error - unrecognized operator";
+    }
+    $("#output").text(result);
   });
 });
